00ac8d57e0b901dcf97f992e24751471aaf46809
LeoRocha <leonardocar.dm@gmail.com>
2019-05-22 19:56:00 -0400

    Add draw and erase logic.

--- src/JavaFX/SIMPController.java
+++ src/JavaFX/SIMPController.java
@@ -32,7 +32,16 @@ public class SIMPController {
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
         canvas.setOnMouseDragged(e -> {
+            double size = Double.parseDouble(brushSize.getText());
+            double x = e.getX() - size / 2;
+            double y = e.getY() - size / 2;
 
+            if (eraser.isSelected())
+                graphicsContext.clearRect(x, y, size, size);
+            else {
+                graphicsContext.setFill(colorPicker.getValue());
+                graphicsContext.fillRect(x, y, size, size);
+            }
         });
     }
 
